e81d16021caec093f35a4be89d6ad9903bfd20b0
Alexander Rozum <roozumaleks@gmail.com>
2024-01-18 21:28:56 +0300

    Создан TaskJson.kt

---
+++ app/src/main/java/com/example/daily/data/external/TaskJson.kt
@@ -0,0 +1,13 @@
+package com.example.daily.data.external
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TaskJson(
+    val id: Int,
+    @SerialName("date_start") val dateStart: Long,
+    @SerialName("date_finish") val dateFinish: Long,
+    val name: String,
+    val description: String
+)
